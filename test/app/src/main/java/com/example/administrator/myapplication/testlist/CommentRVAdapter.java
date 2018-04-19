8a5c4f29e2b75b229f07c731a32a52c8a0940327
Administrator <wmbroid@yeah.net>
2018-04-19 18:38:08 +0800

    edittext.settext引起的textchanged监听必走一次的bug，ratingbar同理-boolean判断

--- test/app/src/main/java/com/example/administrator/myapplication/testlist/CommentRVAdapter.java
+++ test/app/src/main/java/com/example/administrator/myapplication/testlist/CommentRVAdapter.java
@@ -21,6 +21,7 @@ import java.util.List;
  * Created by wmb on 2018/4/19.
  */
 public class CommentRVAdapter extends BaseRecyclerViewAdapter<CommentBean> {
+    private boolean isChanged = true;
     public CommentRVAdapter(Context context, List<CommentBean> beans) {
         super(context, beans);
     }
@@ -32,19 +33,19 @@ public class CommentRVAdapter extends BaseRecyclerViewAdapter<CommentBean> {
 
     @Override
     protected void onBindDataToView(final ViewHolder holder, final CommentBean bean) {
-        initUI(holder, bean, bean.isEdit);
         setListener(holder, bean);
-
+        initUI(holder, bean, bean.isEdit);
     }
 
     private void initUI(ViewHolder holder, CommentBean bean, boolean isEdit) {
+        isChanged = true;
         holder.setText(R.id.ui_tv_name, bean.name);
-        holder.setText(R.id.ui_et_comment, bean.comment);
         RatingBar bar = holder.getView(R.id.ui_ratingBar);
         LayerDrawable drawable = (LayerDrawable) bar.getProgressDrawable();
         drawable.getDrawable(2).setColorFilter(Color.YELLOW, PorterDuff.Mode.SRC_ATOP);
         bar.setRating(bean.score);
         if (isEdit) {
+            holder.setText(R.id.ui_et_comment, bean.comment);
             bar.setIsIndicator(false);
             holder.getView(R.id.ll_comment_tv).setVisibility(View.GONE);
             holder.getView(R.id.rl_comment).setVisibility(View.VISIBLE);
@@ -56,14 +57,18 @@ public class CommentRVAdapter extends BaseRecyclerViewAdapter<CommentBean> {
             holder.setText(R.id.ui_tv_comment, bean.comment);
             bar.setIsIndicator(true);
         }
+        isChanged = false;
     }
 
     private void setListener(final ViewHolder holder, final CommentBean bean) {
+        final EditText et = holder.getView(R.id.ui_et_comment);
+        final RatingBar rb = holder.getView(R.id.ui_ratingBar);
         holder.getView(R.id.btn_commit).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 bean.isEdit = false;
-                initUI(holder, bean, false);
+                //initUI(holder, bean, false);
+                notifyDataSetChanged();
             }
         });
         holder.getView(R.id.btn_reset).setOnClickListener(new View.OnClickListener() {
@@ -71,10 +76,10 @@ public class CommentRVAdapter extends BaseRecyclerViewAdapter<CommentBean> {
             public void onClick(View v) {
                 bean.score = 0;
                 bean.comment = "";
-                initUI(holder, bean, true);
+                //initUI(holder, bean, true);
+                notifyDataSetChanged();
             }
         });
-        final EditText et = holder.getView(R.id.ui_et_comment);
         et.addTextChangedListener(new TextWatcher() {
             @Override
             public void beforeTextChanged(CharSequence s, int start, int count, int after) {
@@ -86,26 +91,29 @@ public class CommentRVAdapter extends BaseRecyclerViewAdapter<CommentBean> {
 
             @Override
             public void afterTextChanged(Editable s) {
-                int end = et.getText().toString().length();
-                int max = 200;
-                if (s.length() > max) {
-                    s.delete(max, end);
-                    et.setText(s);
-                    et.setSelection(max);
-                    Toast.makeText(mContext, "最多输入200个字符", Toast.LENGTH_SHORT).show();
-                }
+                if (!isChanged) {
+                    int end = et.getText().toString().length();
+                    int max = 200;
+                    if (s.length() > max) {
+                        s.delete(max, end);
+                        et.setText(s);
+                        et.setSelection(max);
+                        Toast.makeText(mContext, "最多输入200个字符", Toast.LENGTH_SHORT).show();
+                    }
 
-                bean.comment = s.toString();
-                holder.setText(R.id.ui_tv_limit, s.length() + "/" + max);
-                Log.e("tag" + holder.getCurrentPosition(), s.toString());
+                    bean.comment = s.toString();
+                    holder.setText(R.id.ui_tv_limit, s.length() + "/" + max);
+                    Log.e("tag" + holder.getCurrentPosition(), s.toString()+"来自EditText");
+                }
             }
         });
-        RatingBar rb = holder.getView(R.id.ui_ratingBar);
         rb.setOnRatingBarChangeListener(new RatingBar.OnRatingBarChangeListener() {
             @Override
             public void onRatingChanged(RatingBar ratingBar, float rating, boolean fromUser) {
-                bean.score = rating;
-                Log.e("tag" + holder.getCurrentPosition(), bean.name + "score=" + rating);
+                if (!isChanged) {
+                    bean.score = rating;
+                    Log.e("tag" + holder.getCurrentPosition(), bean.name + "score=" + rating);
+                }
             }
         });
     }
